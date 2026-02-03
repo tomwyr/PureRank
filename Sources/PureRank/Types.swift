4bf662e3cb5eef419352d2e5dc715d6c9c6e7403
Tomasz Wyrowiński <tomwyr@hotmail.com>
2026-02-03 23:01:30 +0100

    Add solo match update logic

--- Sources/PureRank/Types.swift
+++ Sources/PureRank/Types.swift
@@ -4,30 +4,38 @@ struct Player {
   let id: String
   let mean: Double
   let deviation: Double
+
+  var variance: Double { pow(deviation, 2) }
 }
 
 struct Team {
   let players: [Player]
 
-  func mean() -> Double {
+  var mean: Double {
     players.reduce(0.0) { sum, player in sum + player.mean }
   }
 
-  func variance() -> Double {
-    players.reduce(0.0) { sum, player in sum + pow(player.deviation, 2) }
+  var variance: Double {
+    players.reduce(0.0) { sum, player in sum + player.variance }
   }
+}
+
+struct SoloMatch {
+  let playerA: Player
+  let playerB: Player
+  let winnerSide: MatchSide
 }
 
 struct TeamMatch {
   let teamA: Team
   let teamB: Team
-  let winnerSide: Side
+  let winnerSide: MatchSide
 
   var playerCount: Int {
     teamA.players.count + teamB.players.count
   }
+}
 
-  enum Side {
-    case sideA, sideB
-  }
+enum MatchSide {
+  case sideA, sideB
 }
--- Sources/PureRank/UpdateSkill.swift
+++ Sources/PureRank/UpdateSkill.swift
@@ -7,6 +7,26 @@ struct UpdateSkill {
     self.matchVarianceFactor = matchVarianceFactor
   }
 
+  func soloMatch(_ match: SoloMatch) -> SoloMatch {
+    let (winner, loser) =
+      switch match.winnerSide {
+      case .sideA: (match.playerA, match.playerB)
+      case .sideB: (match.playerB, match.playerA)
+      }
+
+    let params = calcUpdateParams(winner: winner, loser: loser)
+    let winnerNew = updatePlayer(winner, params, .win)
+    let loserNew = updatePlayer(loser, params, .lose)
+
+    let (playerANew, playerBNew) =
+      switch match.winnerSide {
+      case .sideA: (winnerNew, loserNew)
+      case .sideB: (loserNew, winnerNew)
+      }
+
+    return SoloMatch(playerA: playerANew, playerB: playerBNew, winnerSide: match.winnerSide)
+  }
+
   func teamMatch(_ match: TeamMatch) -> TeamMatch {
     let (winner, loser) =
       switch match.winnerSide {
@@ -14,7 +34,7 @@ struct UpdateSkill {
       case .sideB: (match.teamB, match.teamA)
       }
 
-    let params = calcUpdateParams(winner: winner, loser: loser, match.playerCount)
+    let params = calcUpdateParams(winner: winner, loser: loser)
     let winnerNew = updateTeam(winner, params, .win)
     let loserNew = updateTeam(loser, params, .lose)
 
@@ -27,19 +47,46 @@ struct UpdateSkill {
     return TeamMatch(teamA: teamANew, teamB: teamBNew, winnerSide: match.winnerSide)
   }
 
-  private func calcUpdateParams(
-    winner: Team, loser: Team,
-    _ playerCount: Int,
-  ) -> MatchUpdateParams {
-    let (wMean, wVariance) = (winner.mean(), winner.variance())
-    let (lMean, lVariance) = (loser.mean(), loser.variance())
+  private func calcUpdateParams(winner: Player, loser: Player) -> MatchUpdateParams {
+    calcUpdateParams(
+      wMean: winner.mean, wVariance: winner.variance,
+      lMean: loser.mean, lVariance: loser.variance,
+      playerCount: 2,
+    )
+  }
 
+  private func calcUpdateParams(winner: Team, loser: Team) -> MatchUpdateParams {
+    calcUpdateParams(
+      wMean: winner.mean, wVariance: winner.variance,
+      lMean: loser.mean, lVariance: loser.variance,
+      playerCount: winner.players.count + loser.players.count,
+    )
+  }
+
+  private func calcUpdateParams(
+    wMean: Double, wVariance: Double,
+    lMean: Double, lVariance: Double,
+    playerCount: Int,
+  ) -> MatchUpdateParams {
     let c = sqrt(wVariance + lVariance + Double(playerCount) * pow(matchVarianceFactor, 2))
     let t = (wMean - lMean) / c
     let vt = StandardNormal.pdf(t) / StandardNormal.cdf(t)
     let wt = vt * (vt + t)
-
     return MatchUpdateParams(c: c, vt: vt, wt: wt)
+  }
+
+  private func updatePlayer(
+    _ player: Player,
+    _ params: MatchUpdateParams,
+    _ delta: MatchUpdateDelta,
+  ) -> Player {
+    let perfVariance = pow(params.c, 2)
+    let variance = pow(player.deviation, 2)
+
+    let meanNew = player.mean + (variance / params.c * params.vt) * delta.rawValue
+    let deviationNew = sqrt(variance * (1 - (variance / perfVariance * params.wt)))
+
+    return Player(id: player.id, mean: meanNew, deviation: deviationNew)
   }
 
   private func updateTeam(
@@ -47,17 +94,9 @@ struct UpdateSkill {
     _ params: MatchUpdateParams,
     _ delta: MatchUpdateDelta,
   ) -> Team {
-    let players = team.players.map { player in
-      let perfVariance = pow(params.c, 2)
-      let variance = pow(player.deviation, 2)
-
-      let meanNew = player.mean + (variance / params.c * params.vt) * delta.rawValue
-      let deviationNew = sqrt(variance * (1 - (variance / perfVariance * params.wt)))
-
-      return Player(id: player.id, mean: meanNew, deviation: deviationNew)
-    }
-
-    return Team(players: players)
+    Team(
+      players: team.players.map { updatePlayer($0, params, delta) }
+    )
   }
 }
 
